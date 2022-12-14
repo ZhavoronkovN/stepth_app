5e0afdf7b0a494c9e6b01c23606ed32aece974b8
Nikita <n0962783666@gmail.com>
2022-12-14 08:42:00 +0200

    Add sleep to build server before running client

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 kill $(ps -A | grep "stepth-server" | awk '{print $1}')
 python3 -m grpc_tools.protoc -I ./stepth-protos/ --python_out=./stepthui_py/ --grpc_python_out=./stepthui_py/ ./stepth-protos/stepth.proto
 cargo run --manifest-path ./stepth-server/Cargo.toml &
+sleep 1
 python3 ./stepthui_py/main.py
 kill $(ps -A | grep "stepth-server" | awk '{print $1}')
